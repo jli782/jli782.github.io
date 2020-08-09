b5cc402a8744cc602ce652dfbb3923dc2a080fec
Jeffrey Li <jli588@myseneca.ca>
2020-08-09 15:47:47 -0400

    updated forms

--- form.js
+++ form.js
@@ -3,15 +3,17 @@
     //reveal comment textbox if selected
     var radioSelections = document.getElementsByName('selection');
     for (var i = 0; i < radioSelections.length; i++) {
-      if (radioSelections[i].id != 'contact-order-info') {
+      if (radioSelections[i].id !== 'contact-order-info') {
         radioSelections[i].addEventListener('click', () => {
           this.checked = true;
           document.querySelector('.contact-order-problem-text').style.display = 'none';
+          document.querySelector('#contact-order-problem-text').required = false;
         });
       } else {
         radioSelections[i].addEventListener('click', () => {
           this.checked = true;
           document.querySelector('.contact-order-problem-text').style.display = 'inherit';
+          document.querySelector('#contact-order-problem-text').required = true;
         });
       }
     }
@@ -23,6 +25,31 @@
         goUpBtn.style.display = 'block';
       } else {
         goUpBtn.style.display = 'none';
+      }
+    });
+
+    //check validity of user input
+    let form = document.querySelector('#contact-email-form');
+    form.addEventListener('submit', function(event) {
+      if (!form.checkValidity()) {
+        form.classList.add('was-validated');
+        event.preventDefault();
+        return false;
+      }
+      return true;
+    });
+
+    //some error messages
+    let email = document.querySelector('#contact-email-info');
+    email.addEventListener('input', () => {
+      if (this.checkValidity.patternMismatch) {
+        this.setCustomValidity('Enter a valid email!');
+      }
+    });
+    let postalCode = document.querySelector('#contact-postal-code-info');
+    postalCode.addEventListener('input', () => {
+      if (this.checkValidity.patternMismatch) {
+        this.setCustomValidity('Enter a valid Canadian Postal Code!');
       }
     });
   } //
